7e7deb732be3584c760b5239a0bdc2df4ea5511d
ShayakC98 <37727400+ShayakC98@users.noreply.github.com>
2019-08-26 20:33:36 +0530

    Small changes in docs

--- QLearningDriver.js
+++ QLearningDriver.js
@@ -1,8 +1,20 @@
 /*
 
-The Update Rule for Q learning model
+Our Q learning model has 3 possible states:
+Left, Middle, Right
 
+Now for each state our model has 2 possible actions:
+To go either Left, or To go either Right.
+
+so the Shape of our Q-table will be of 3 x 2
+
+		Left 	Middle 	Right
+Left    [val 	val    	val
+Right    val	val 	val]
+
+The Update Rule for Q learning model:
 Q[state, action] = Q[state, action] + lr * (reward + gamma * np.max(Q[new_state, :]) — Q[state, action])
+
 
 */
 
